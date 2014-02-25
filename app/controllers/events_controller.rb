2767f30c6af16b0daec883a77c4af9fce5818603
Dakota B <dbarkwill@gmail.com>
2014-02-25 00:31:12 -0500

    Assignment 4 Updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  layout "application-sidebar", only: [:show]
-
   # GET /events
   # GET /events.json
   def index
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
-  layout "application-sidebar", only: [:show]
+  
 
   # GET /locations
   # GET /locations.json
